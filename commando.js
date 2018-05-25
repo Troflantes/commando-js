83bb9a624fcd05de90b36d9b4da960efaabd3721
bycat <kubilayuzun08@gmail.com>
2018-05-25 17:06:45 +0300

    1.0 Yayında!

---
+++ commando.js
@@ -0,0 +1,27 @@
+const { CommandoClient } = require('discord.js-commando');
+const config = require('./config.json')
+const path = require('path');
+const sqlite = require('sqlite');
+
+const client = new CommandoClient({
+    commandPrefix: config.botprefix,
+    unknownCommandResponse: false,
+    owner: config.owner.id,
+    disableEveryone: true
+});
+
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ['group1', 'Our First Command Group']
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.on('ready', () => {
+    console.log('BOT: Bot Started');
+    client.user.setActivity('Commando! Go');
+});
+
+client.login(config.bot.token);
